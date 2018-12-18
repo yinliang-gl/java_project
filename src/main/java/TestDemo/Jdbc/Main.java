113bb64795a06f0b0fec9330e0942effec04af93
yinliang <muyuxuebao@163.com>
2018-12-18 19:56:11 +0800

    aaaaaaa

--- src/main/java/TestDemo/Jdbc/Main.java
+++ src/main/java/TestDemo/Jdbc/Main.java
@@ -1,7 +1,63 @@
 package TestDemo.Jdbc;
 
+import com.mysql.jdbc.Connection;
+import com.mysql.jdbc.PreparedStatement;
+
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class Main {
+
+    private static Connection getConnection() {
+        String driver = "com.mysql.jdbc.Driver";
+        String url = "jdbc:mysql://192.168.55.170:3358/";
+        String username = "root";
+        String password = "123456";
+        Connection conn = null;
+        try {
+            Class.forName(driver); //classLoader,加载对应驱动
+            conn = (Connection) DriverManager.getConnection(url, username, password);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return conn;
+    }
+
+    private static Integer select() {
+        Connection conn = getConnection();
+        String sql = "select * from dsp.ad limit 10";
+        PreparedStatement pstmt;
+        try {
+            pstmt = (PreparedStatement) conn.prepareStatement(sql);
+            ResultSet rs = pstmt.executeQuery();
+            int col = rs.getMetaData().getColumnCount();
+
+            for (int i = 1; i <= col; i++) {
+                System.out.print(rs.getMetaData().getColumnName(i) + "\t");
+            }
+            System.out.println();
+
+            System.out.println("============================");
+            while (rs.next()) {
+                for (int i = 1; i <= col; i++) {
+                    System.out.print(rs.getString(i) + "\t");
+                    if ((i == 2) && (rs.getString(i).length() < 16)) {
+                        System.out.print("\t");
+                    }
+                }
+                System.out.println("");
+            }
+            System.out.println("============================");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
     public static void main(String[] args) {
-        System.out.println("TTTTTT");
+        select();
     }
 }
